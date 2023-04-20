4413988ef37e7ed3d940638fed2e65f95e2bed10
Yaxuan Huang <yaxuanhuang0710@gmail.com>
2023-04-20 01:22:04 -0400

    fix the problem of button floating

--- static/js/app.js
+++ static/js/app.js
@@ -82,82 +82,90 @@ function generate_selected_screenshot() {
 
 
 //highlight selection, next step:optimize the selection element layer, like Table S1.T1, optimize the button format and area, optimize the sensitivity of selection(like mouseup)
-document.onselectionchange = function () {
-    const selectedText = window.getSelection();
-    if (selectedText.toString().trim()) {
-      // Remove any previous report buttons
-      const previousButtons = document.querySelectorAll(
-        "button[id='small-report-button']"
-      );
-      for (const button of previousButtons) {
-        button.remove();
-      }
+document.addEventListener("mouseup", function (event) {
+  const selectedText = window.getSelection();
+  if (selectedText.toString().trim()) {
+    // Remove any previous report buttons
+    const previousButtons = document.querySelectorAll(
+      "button[id='small-report-button']"
+    );
+    for (const button of previousButtons) {
+      button.remove();
+    }
 
-      // Determine the position of the selected text
-      const range = selectedText.getRangeAt(0);
-      const rect = range.getBoundingClientRect();
+    // Determine the position of the selected text
+    const range = selectedText.getRangeAt(0);
+    const rect = range.getBoundingClientRect();
 
-      if (selectedText.type === 'Range') {
-        var anchorNode = selectedText.anchorNode;
-        var parentNode = anchorNode.parentNode;
-        var id = parentNode.id;
-        var classList = parentNode.classList;
-        //if there is no id, than use class to identify
-        var elementIdentifier = id || classList[0] || 'Unknown';
-    
-        //get the topLayer of id
-        if (elementIdentifier.startsWith('S0')) {
-          var topLayer = id ? id.split('.').slice(0, 2).join('.') : classList[0];
-          }
-        else{
-          var topLayer = id ? id.split('.')[0] : classList[0];
+    if (selectedText.type === 'Range') {
+      var anchorNode = selectedText.anchorNode;
+      var parentNode = anchorNode.parentNode;
+      var id = parentNode.id;
+      var classList = parentNode.classList;
+      //if there is no id, than use class to identify
+      var elementIdentifier = id || classList[0] || 'Unknown';
+
+      //get the topLayer of id
+      if (elementIdentifier.startsWith('S0')) {
+        var topLayer = id ? id.split('.').slice(0, 2).join('.') : classList[0];
         }
-    
-        //print the current element identifier and its toplayer
-        console.log('Selected element identifier:', elementIdentifier);
-        console.log('Top layer identifier:', topLayer);
-      }
-      
-      // Show the report button
-      const smallReportButton = document.createElement("button");
-      smallReportButton.id = "small-report-button";
-      smallReportButton.type='button'
-      smallReportButton.className='btn btn-secondary btn-sm'
-      smallReportButton.style.backgroundColor='#b31b1b'
-      smallReportButton.innerHTML = "Report";
-      smallReportButton.style.position = "fixed";
-      smallReportButton.style.left = `${rect.left + rect.width / 2}px`;
-
-      // Check if there is enough space above the selected text
-      if (rect.top > 40) {
-        smallReportButton.style.top = `${rect.top}px`;
-        smallReportButton.style.transform = "translate(-50%, -100%)";
-      } else {
-        smallReportButton.style.top = `${rect.top}px`;
-        smallReportButton.style.transform = "translate(-50%, 100%)";
+      else{
+        var topLayer = id ? id.split('.')[0] : classList[0];
       }
 
-      document.body.appendChild(smallReportButton);
+      //print the current element identifier and its toplayer
+      console.log('Selected element identifier:', elementIdentifier);
+      console.log('Top layer identifier:', topLayer);
+    }
 
-      // Handle the report button click event
-      smallReportButton.addEventListener("click", function () {
-        //createReportBox();
-        generate_selected_screenshot()
-        modal.style.display = 'block'
-        smallReportButton.remove();
-      });
+    // Show the report button
+    const smallReportButton = document.createElement("button");
+    smallReportButton.id = "small-report-button";
+    smallReportButton.type = 'button';
+    smallReportButton.className = 'btn btn-secondary btn-sm';
+    smallReportButton.style.backgroundColor = '#b31b1b';
+    smallReportButton.innerHTML = "Report";
+    smallReportButton.style.position = "fixed";
+    smallReportButton.style.left = `${rect.left + rect.width / 2}px`;
+
+    // Check if there is enough space above the selected text
+    if (rect.top > 40) {
+      smallReportButton.style.top = `${rect.top}px`;
+      smallReportButton.style.transform = "translate(-50%, -100%)";
     } else {
-      // Remove the report button
-      const smallReportButton = document.querySelector(
-        "button[id='small-report-button']"
-      );
-      if (smallReportButton) {
-        smallReportButton.remove();
+      smallReportButton.style.top = `${rect.top}px`;
+      smallReportButton.style.transform = "translate(-50%, 100%)";
+    }
+
+    document.body.appendChild(smallReportButton);
+
+    // Handle the report button click event
+    smallReportButton.addEventListener("click", function () {
+      //createReportBox();
+      generate_selected_screenshot()
+      modal.style.display = 'block'
+      smallReportButton.remove();
+    });
+
+    // Handle the window scroll event
+    window.onscroll = function() {
+      if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        smallReportButton.style.display = "none";
+      } else {
+        smallReportButton.style.display = "block";
       }
     }
-  };
+  } else {
+    // Remove the report button
+    const smallReportButton = document.querySelector(
+      "button[id='small-report-button']"
+    );
+    if (smallReportButton) {
+      smallReportButton.remove();
+    }
+  }
+});
 
-  
   //click the button to generate the screenshot, next step: use external library to fasten the process
   // This is different from the screenshot of the selected area.
   document.getElementById("take-screenshot").addEventListener("click", function() {
