a0a40c9ea90f545338da32f3008435aaa8293f40
CYBuckeye <78572352+YiChen8185@users.noreply.github.com>
2023-05-26 20:00:51 +1200

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -64,6 +64,9 @@ function addBugReportForm() {
   descriptionTextarea.setAttribute("style", "height: 80px;");
 
   // Create the file input field
+  var fileDiv = document.createElement("div");
+  fileDiv.setAttribute("class", "mb-3");
+
   var fileLabel = document.createElement("label");
   fileLabel.setAttribute("for", "file");
   fileLabel.setAttribute("class", "form-label");
@@ -74,6 +77,9 @@ function addBugReportForm() {
   fileInput.setAttribute("class", "form-control");
   fileInput.setAttribute("id", "file");
   fileInput.setAttribute("name", "file");
+
+  fileDiv.appendChild(fileLabel);
+  fileDiv.appendChild(fileInput);
 
   // Create the take screenshot button
   var takeScreenshotButton = document.createElement("button");
@@ -94,6 +100,16 @@ function addBugReportForm() {
   screenshotImage.setAttribute("class", "img-fluid");
   screenshotImage.setAttribute("src", "");
 
+  // Notification ?
+  var notification = document.createElement("div");
+  notification.setAttribute("id", "notification");
+  notification.setAttribute("class", "alert alert-success mt-3");
+  notification.setAttribute("role", "alert");
+  notification.setAttribute("style", "display: none;");
+  notification.appendChild(document.createTextNode("Bug report submitted successfully!"));
+
+  
+
   // Create the modal footer
   var modalFooter = document.createElement("div");
   modalFooter.setAttribute("class", "modal-footer d-flex justify-content-end");
@@ -106,18 +122,14 @@ function addBugReportForm() {
   submitButton.setAttribute("style", "background-color: #b31b1b;");
   submitButton.appendChild(document.createTextNode("Submit"));
 
-  // Create a container div for the buttons
-  var buttonsContainer = document.createElement("div");
-  buttonsContainer.setAttribute("class", "d-flex justify-content-between");
-
-  // Append the elements to their respective parents
+  
   modalBody.appendChild(descriptionLabel);
   modalBody.appendChild(descriptionTextarea);
-  modalBody.appendChild(fileLabel);
-  modalBody.appendChild(fileInput);
+  modalBody.appendChild(fileDiv);
   modalBody.appendChild(takeScreenshotButton);
   modalBody.appendChild(screenshotInput);
   modalBody.appendChild(screenshotImage);
+  modalBody.appendChild(notification);
 
   modalFooter.appendChild(submitButton);
 
@@ -140,9 +152,9 @@ function addBugReportForm() {
 document.addEventListener("DOMContentLoaded", () => {
   addBugReportForm();
   const modal = document.getElementById('myForm')
-  //const close = modal.querySelector('.close')
-  const close = modal.querySelector('.btn-close')
-
+  //const close = modal.querySelector('.btn-close')
+  const close = modal.getElementById('closeForm')
+  
   let isCommandKeyDown = false;
   // Control Report Box based on the button
   document.addEventListener('keydown', function (event) {
