b6f6c35ac922875d1017dd6282f70a0f0c618961
Mark Nazzaro <marknazzaro2@gmail.com>
2023-07-03 20:45:05 -0400

    Things look good

--- static/js/feedbackOverlay.js
+++ static/js/feedbackOverlay.js
@@ -14,8 +14,6 @@ function detectColorScheme() {
       document.documentElement.setAttribute("data-theme", "light"); } 
 }
 
-detectColorScheme();
-
 function toggleColorScheme(){
     var current_theme = localStorage.getItem("ar5iv_theme");
     if (current_theme) {
@@ -97,44 +95,6 @@ function addBugReportForm() {
     descriptionTextarea.setAttribute("maxlength", "1000"); // Set the maximum length to 200 characters
     descriptionTextarea.setAttribute("placeholder","1000 characters maximum");
 
-    // Create the file input field
-    const fileDiv = document.createElement("div");
-    fileDiv.setAttribute("class", "mb-3");
-
-    const fileLabel = document.createElement("label");
-    fileLabel.setAttribute("for", "file");
-    fileLabel.setAttribute("class", "form-label");
-    fileLabel.appendChild(document.createTextNode("Image File(optional):"));
-
-    const fileInput = document.createElement("input");
-    fileInput.setAttribute("type", "file");
-    fileInput.setAttribute("class", "form-control");
-    fileInput.setAttribute("id", "file");
-    fileInput.setAttribute("name", "file");
-    fileInput.setAttribute("accept", "image/*"); // Specify the allowed file types
-
-    fileDiv.appendChild(fileLabel);
-    fileDiv.appendChild(fileInput);
-
-    // Create the take screenshot button
-    const takeScreenshotButton = document.createElement("button");
-    takeScreenshotButton.setAttribute("type", "button");
-    takeScreenshotButton.setAttribute("class", "btn btn-outline-secondary");
-    takeScreenshotButton.setAttribute("id", "take-screenshot");
-    takeScreenshotButton.appendChild(document.createTextNode("Take Screenshot"));
-
-    // Create the hidden input field for the screenshot
-    const screenshotInput = document.createElement("input");
-    screenshotInput.setAttribute("type", "hidden");
-    screenshotInput.setAttribute("id", "screenshot");
-    screenshotInput.setAttribute("name", "screenshot");
-
-    // Create the screenshot image element
-    const screenshotImage = document.createElement("img");
-    screenshotImage.setAttribute("id", "screenshot-image");
-    screenshotImage.setAttribute("class", "img-fluid");
-    screenshotImage.setAttribute("src", "");
-
     // Create the modal footer
     const modalFooter = document.createElement("div");
     modalFooter.setAttribute("class", "modal-footer d-flex justify-content-end");
@@ -154,10 +114,6 @@ function addBugReportForm() {
     // Append the elements to their respective parents
     modalBody.appendChild(descriptionLabel);
     modalBody.appendChild(descriptionTextarea);
-    modalBody.appendChild(fileDiv);
-    modalBody.appendChild(takeScreenshotButton);
-    modalBody.appendChild(screenshotInput);
-    modalBody.appendChild(screenshotImage);
 
     modalFooter.appendChild(submitButton);
 
@@ -179,7 +135,7 @@ function addBugReportForm() {
 }
 
 // Create SRButton that can open the report modal
-function addSRButton() {
+function addSRButton(modal) {
     const contents = document.querySelectorAll('p, svg, figure, .ltx_title, .ltx_authors');
     const buttons = [];
 
@@ -195,13 +151,10 @@ function addSRButton() {
         button.style.display = "none";
         button.textContent = "Report Bug";
 
-        // handle the focus
         button.onfocus = () => previousFocusElement = document.activeElement;
 
-        // Add click event listener to the hidden button
-        // TODO: Make function for showing modal
         button.onclick = (e) => {
-            showModal('screen reader')
+            showModal(modal);
             e.preventDefault();
         };
 
@@ -214,7 +167,7 @@ function addSRButton() {
     return buttons;
 }
 
-function showModal (modal, initiationMode) {
+function showModal (modal) {
     modal.style.display = 'block';
     modal.setAttribute('tabindex', '-1'); // Ensure the modal is focusable
     modal.focus();
@@ -240,39 +193,94 @@ function hideButtons (buttons) {
 const handleKeyDown = (e, modal, buttons) => {
     const ctrlOrMeta = e.metaKey || e.ctrlKey;
 
-    console.log(e.code);
-    console.log(e.shiftKey);
     if (e.shiftKey && e.code === 'KeyB') { 
-        console.log('Got HERe');
         showButtons(buttons);
-    }
-
-    if (ctrlOrMeta && (e.key === '/' || e.key === '?')) {
-        showModal(modal, 'shortcut')
-    }
-
-    if (ctrlOrMeta && (e.key === '}' || e.key === ']')) {
+    } else if (ctrlOrMeta && (e.key === '/' || e.key === '?')) {
+        showModal(modal)
+    } else if (ctrlOrMeta && (e.key === '}' || e.key === ']')) {
         hideModal(modal);
     }
 }
 
 //The highlight initiation way
-function handleHighlight (e, modal) {
+function handleMouseUp (e, smallButton) {
         if (e.target.id === "small-report-button") 
             return;
-
-        // hideButtons(buttons);
-
-        const selection = window.getSelection();
-
-        if (!selection.isCollapsed) {
-            showSmallButton(selection, modal);
-        }
+        if (!window.getSelection().isCollapsed)
+            showSmallButton(smallButton);
+        else hideSmallButton(smallButton);
 }
 
-    //get the info of selected text, reset the global constiable's value
-function generate_selected_text(selection, range) {
-    if (selection.type === 'Range') {
+function createSmallButton (modal) {
+    const smallReportButton = document.createElement('button');
+    smallReportButton.id = 'small-report-button';
+    smallReportButton.type = 'button';
+    smallReportButton.className = 'btn btn-secondary btn-sm';
+    smallReportButton.style.backgroundColor = '#b31b1b';
+    smallReportButton.textContent = 'Report';
+    smallReportButton.style.position = 'fixed';
+
+    document.body.appendChild(smallReportButton);
+
+    smallReportButton.onclick = (e) => {
+        showModal(modal); // do something with window.getSelection()
+    }
+
+    smallReportButton.addEventListener("focusout", function (e) {
+        hideSmallButton(this);
+    });
+
+    return smallReportButton;
+}
+
+// Display the smallButton for bug report, click and scroll included
+function showSmallButton(smallReportButton) {
+    selection = window.getSelection();
+
+    const range = selection.getRangeAt(0);
+    const rect = range.getBoundingClientRect();
+
+    smallReportButton.style.left = `${rect.left + rect.width / 2}px`;
+
+    // Check if there is enough space above the selected text
+    smallReportButton.style.top = `${rect.top}px`;
+    smallReportButton.style.transform = 'translate(-50%, -100%)';
+
+    smallReportButton.style.display = 'inline';
+}
+
+function hideSmallButton (smallReportButton) {
+    smallReportButton.style.display = 'none';
+}
+
+//submit to the backend, next step: finish
+function submitBugReport (e) {
+    //document.getElementById('notification').style = 'display: block';
+    const formData = new FormData();
+
+    // Canonical URL
+    ARXIV_ABS_PATH = 'https://arxiv.org/abs/';
+    const arxivIdv = window.location.pathname.split('/')[2]; // pathname ex: '/html/2306.16433v1/2306.16433v1.html'
+    const canonicalURL = ARXIV_ABS_PATH + arxivIdv;
+
+    // const user_info = "account:yc2455 contact:@cornll.edu "
+
+    // Report Time
+    const currentTime = Date.now();
+
+    // Browser Version
+    const userAgent = navigator.userAgent;
+    const browser = userAgent.match(/(firefox|edge|opr|chrome|safari)[\/]([\d.]+)/i)
+    const browserName = browser[1];
+    const browserVersion = browser[2];
+    const browserInfo = browserName + '/' + browserVersion;
+
+    // Relevant Selection
+    const selection = window.getSelection();
+    let elementIdentifier = 'Unknown';
+    let topLayer = 'Unknown';
+    let dataURI = 'Unknown';
+    if (!selection.isCollapsed) {
         const anchorNode = selection.anchorNode;
         const parentNode = anchorNode.parentNode;
         const id = parentNode.id;
@@ -292,141 +300,29 @@ function generate_selected_text(selection, range) {
         container.appendChild(range.cloneContents());
         const selectedHtml = container.innerHTML;
         // Use the selected text to generate the dataURI
-        saved_dataURI = "data:text/html;charset=utf-8," + encodeURIComponent(selectedHtml);
+        dataURI = "data:text/html;charset=utf-8," + encodeURIComponent(selectedHtml);
     }
-}
+    
+    const dataDescription = document.getElementById('description').value;
 
-//remove the previous small button
-function removePreviousButton() {
-    const previousButtons = document.querySelectorAll(
-        "button[id='small-report-button']"
-    );
-    for (const button of previousButtons) {
-        button.remove();
-    }
-}
-
-//Generate the smallButton for bug report, click and scroll included
-function showSmallButton(selection, modal) {
-    //create small button
-    const range = selection.getRangeAt(0);
-    const rect = range.getBoundingClientRect();
-
-    const smallReportButton = document.createElement("button");
-    smallReportButton.id = "small-report-button";
-    smallReportButton.type = 'button';
-    smallReportButton.className = 'btn btn-secondary btn-sm';
-    smallReportButton.style.backgroundColor = '#b31b1b';
-    smallReportButton.innerHTML = "Report";
-    smallReportButton.style.position = "fixed";
-    smallReportButton.style.left = `${rect.left + rect.width / 2}px`;
-
-    // Check if there is enough space above the selected text
-    if (rect.top > 40) {
-        smallReportButton.style.top = `${rect.top}px`;
-        smallReportButton.style.transform = "translate(-50%, -100%)";
-    } else {
-        smallReportButton.style.top = `${rect.top}px`;
-        smallReportButton.style.transform = "translate(-50%, 100%)";
-    }
-    document.body.appendChild(smallReportButton);
-
-    //click small button
-    smallReportButton.addEventListener("click", function () {
-        initiationWay='highlight';
-        generate_selected_text(selection,range);
-        modal.style.display = 'block';
-        smallReportButton.remove();
-    });
-
-    smallReportButton.addEventListener("focusout", function (e) {
-        this.parentNode.removeChild(this);
-    });
-
-    smallReportButton.focus();
-
-    // Handle the window scroll event
-    window.onscroll = function () {
-        if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-        smallReportButton.style.display = "none";
-        } else {
-        smallReportButton.style.display = "block";
-        }
-    }
-}
-
-//submit report
-function handleFormSubmit() {
-    document.getElementById("myFormContent").addEventListener("submit", function (event) {
-        event.preventDefault();
-        submitBugReport();
-    });
-}
-
-//submit to the backend, next step: finish
-function submitBugReport() {
-    //document.getElementById('notification').style = 'display: block';
-    const formData = new FormData();
-    const metaElement = document.querySelector('meta[property="og:url"]');
-    const article_url = metaElement ? metaElement.getAttribute('content') : null;
-    const user_info = "account:yc2455 contact:@cornll.edu "
-    //report time
-    const currentTime = new Date();
-    //browser version
-    const userAgent = navigator.userAgent;
-    const browser = userAgent.match(/(firefox|edge|opr|chrome|safari)[\/]([\d.]+)/i)
-    const browserName = browser[1];
-    const browserVersion = browser[2];
-    const browserInfo = browserName + '/' + browserVersion;
-
-    //device info(system)--give up for now
-    //conversion report link
-    const start_index = article_url.lastIndexOf('/') + 1;
-    const number = article_url.substring(start_index);
-    const conversion_report = "https://ar5iv.labs.arxiv.org/log/" + number;
-    //source file link
-    const source_file = "https://arxiv.org/abs/" + number;
-    //location-low
-    //location-high
-    const data_description = document.getElementById("description").value;
-    const screenshotImage = document.getElementById("screenshot").value;
-    const attachment = document.querySelector('#file').files[0];
     // add to the form data
-    formData.append('article_url', article_url);
-    formData.append('user_info', user_info);
+    formData.append('template', 'bug_report.md'); // TODO: Change this to a template with fields matching the ones below
+    formData.append('canonicalURL', canonicalURL);
+    formData.append('conversionURL', window.location.origin + window.location.pathname);
+    formData.append('dataURI', dataURI);
     formData.append('reportTime', currentTime);
     formData.append('browserInfo', browserInfo)
-    formData.append('conversion_report', conversion_report)
-    formData.append('source_file', source_file)
-    formData.append('description', data_description);
-    formData.append('attachment', attachment);
-    formData.append('screenshotImage', screenshotImage);
-    formData.append('url', saved_dataURI);
-    formData.append('location_low', elementIdentifier);
-    formData.append('location_high', topLayer)
-    //console.log(initiationWay);
-    formData.append('initiationWay',initiationWay);
-    fetch('/', {
-        method: 'POST',
-        body: formData
-    })
-    .then(function (response) {
-        if (response.ok) {
-            alert('Report submitted');
-            document.getElementById("screenshot-image").style = "display: none";
-            document.querySelector('#myFormContent').reset(); // Reset the form
-            modal.style.display = 'none'
-            if(previousFocusElement){
-                previousFocusElement.focus();
-            }
-        } else {
-            alert('Error occurs when submitting report');
-        }
-    })
-    .catch(function (error) {
-        console.log(error);
-        alert('Error occurs when submitting report');
-    });
+    formData.append('description', dataDescription);
+    formData.append('locationLow', elementIdentifier);
+    formData.append('locationHigh', topLayer)
+
+    const GITHUB_BASE_URL = 'https://github.com/arXiv/html_feedback/issues/new?' 
+    const queryString = new URLSearchParams(formData).toString()
+    const link = GITHUB_BASE_URL + queryString;
+
+    window.open(link, '_blank');
+
+    e.preventDefault();
 }
 
 function handleClickOutsideModal(e, modal) {
@@ -434,23 +330,25 @@ function handleClickOutsideModal(e, modal) {
         modal.style.display = 'none';
 }
 
+
+// RUN THIS CODE ON INITIALIZE
+detectColorScheme();
 document.addEventListener("DOMContentLoaded", () => {
 
     const modal = addBugReportForm();
-    const reportButtons = addSRButton();
+    const reportButtons = addSRButton(modal);
+    const smallReportButton = createSmallButton(modal);
 
     document.onkeydown = (e) => handleKeyDown(e, modal, reportButtons);
     document.onclick = (e) => handleClickOutsideModal(e, modal);
-    document.onmouseup = (e) => handleHighlight(e, modal);
+    document.onmouseup = (e) => handleMouseUp(e, smallReportButton);
     // const close = modal.querySelector('.btn-close')
 
+    // Handle the window scroll event
+    window.onscroll = () => {
+        if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40)
+            hideSmallButton(smallReportButton);
+    }
 
-    // const isCommandKeyDown = false;
-    // let saved_dataURI;
-    // let elementIdentifier;
-    // let topLayer;
-    // let previousFocusElement;
-    // let initiationWay;
-
-    handleFormSubmit();
+    document.getElementById('myFormContent').onsubmit = submitBugReport;
 });
